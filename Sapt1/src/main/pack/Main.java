ab630ca8eb119905ee71fcd5a615b14486c47ab6
Andreea <Andreea@DESKTOP-I2B25MD>
2021-02-25 09:34:34 +0200

    Primul commit

--- Sapt1/src/main/pack/Main.java
+++ Sapt1/src/main/pack/Main.java
@@ -1,5 +1,6 @@
 package main.pack;
 
+import ro.ase.cts.zoo.Giraffe;
 import ro.ase.cts.zoo.Zoo;
 import ro.ase.cts.zoo.Zookeper;
 
@@ -11,6 +12,20 @@ public class Main {
 		Zookeper keeper = new Zookeper("Dani");
 		Zoo zoo = new Zoo();
 		zoo.setZookeper(keeper);
+	
+	Giraffe giraffe=new Giraffe("Girafa",10);
+	Giraffe secondGiraffe=new Giraffe("Girafa2",15,200);
+	
+	zoo.addAnimal(giraffe);
+	zoo.addAnimal(secondGiraffe);
+	
+	zoo.feedAllAnimals();
+	
+	Zebra z1=new Zebra("Zebra1",7);
+	Zebra z2=new Zebra("Zebra2",9,12);
+	
+	zoo.addAnimal(z1);
+	zoo.addAnimal(z2);
 	}
 
 }
