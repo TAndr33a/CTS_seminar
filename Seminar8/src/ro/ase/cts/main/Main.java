3938f79b91e8df22dee76f98a2557e5f8fb91df9
Andreea <Andreea@DESKTOP-I2B25MD>
2021-04-17 13:56:45 +0300

    Proxy

---
+++ Seminar8/src/ro/ase/cts/main/Main.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.main;
+
+import ro.ase.cts.proxy.OperatorRezervari;
+import ro.ase.cts.proxy.ProxyOperatorRezervari;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OperatorRezervari operator=new OperatorRezervari();
+		operator.realizeazaRezervare("Andrei", 1);
+		
+		ProxyOperatorRezervari proxy=new ProxyOperatorRezervari(operator,4);
+		proxy.realizeazaRezervare("Ion", 1);
+		proxy.realizeazaRezervare("Mihai", 4);
+	}
+	
+	
+
+}
